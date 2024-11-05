e846337810258c879c8265185f4a89fa5cbf0d5f
Roman Khancha <joromeax@gmail.com>
2024-11-05 19:24:37 +0200

    Initial Commit

---
+++ MoneyMap/MoneyMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyMapApp.swift
+//  MoneyMap
+//
+//  Created by Roman Khancha on 05.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct MoneyMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
